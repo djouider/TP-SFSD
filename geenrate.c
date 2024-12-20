9cbe7de06ec76dd6f4884962869190f56ac045a1
mokhtar <nm_djouider@esi.dz>
2024-12-20 18:42:37 +0100

    optimized the search for doubles

--- geenrate.c
+++ geenrate.c
@@ -38,7 +38,7 @@ int generate_random_id(int *list){
     int n,i;bool f;
     do {
         n = 110000 +  rand() % (880001);
-        i = n % 880000;
+        i = n - 110000;
         //n = 1 +  rand() % (30);
     }while(list[i] == 1);
     //printf("\n");
@@ -46,12 +46,10 @@ int generate_random_id(int *list){
     return n;
 }
 
-int generate_random_id2(int *list, int *len,bool ok){
+int generate_random_id2(int *list, int *len){
     int n;bool f;
-    MTRand r = seedRand(1337);
     do {
-        n = 110000 + genRandLong(&r) % (880001);
-        //n = 1 +  rand() % (30);
+        n = 1 +  rand() % (880001);
         f = find_list(list,*len,n);
     }while(f);
     //printf("\n");
@@ -60,31 +58,44 @@ int generate_random_id2(int *list, int *len,bool ok){
     return n;
 }
 
+void check_double(int *list,int length){
+    int i,j;
+    bool found;
+    for (i=0;i<length;i++){
+        for (j=i+1;j<length;j++){
+            if (list[i] == list[j]){
+                printf("i=%d j=%d value=%ddjnhgfbdvc\n",i,j,list[i]);
+            }
+        }
+    }
+}
+
 int main() {
-    printf("x\n");
-    int list[1], len =0,i,N;
+    int len =0,i,N;
     int *list2 = (int*)malloc(880000*sizeof(int));
-    printf("x\n");
     bool ok;
     clock_t start , end;
     printf("pick up a number\n");
     scanf("%d",&N);
+    int list[N];
     for (i=0;i<N;i++){
         list2[i] = 0;
     }
     start = clock();
+    //* comparing between the two methods;
+    //method 1:
     for (i=0;i<N;i++){
         len = generate_random_id(list2);
-        printf("i=%d i/b=%d random=%d",i+1,i/1024,len);
-        printf("true?=%x\n",list2[len % 880000] == 1);
+        printf("i=%d i/b=%d random=%d\n",i+1,i/1024,len);
+        list[i] = len;
     }
     end = clock();
     printf("time took %f\n",((double) (end - start)) / CLOCKS_PER_SEC);
-    for (i=0;i<N;i++){
-        if (list2[i]==1){
-        printf("i=%d\n",i);
-        }
-    }
+
+    start = clock();
+    check_double(list,N);
+    end = clock();
+    printf("time took %f\n",((double) (end - start)) / CLOCKS_PER_SEC);
    /*for (i=0;i<32767;i++){
         generate_random_id(list,&len,ok);
     }*/
